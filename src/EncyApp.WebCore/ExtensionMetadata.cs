c5113b550c081b53e637ee62db02ac0a7286bf48
Kwaku Boamah <kwaku.boamah@petratrust.com>
2020-10-27 13:29:23 +0000

    add some styles to app

--- src/EncyApp.WebCore/ExtensionMetadata.cs
+++ src/EncyApp.WebCore/ExtensionMetadata.cs
@@ -10,11 +10,14 @@ namespace EncyApp.WebCore
     {
         public IEnumerable<StyleSheet> StyleSheets => new StyleSheet[]
         {
-            new StyleSheet("/Styles.encyapp.webcore.css", 100)
+            new StyleSheet("//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css", 100),
+            new StyleSheet("/Styles.encyapp.webcore.css", 200)
         };
 
         public IEnumerable<Script> Scripts => new Script[] {
-            new Script("/Scripts.encyapp.webcore.js", 100)
+            new Script("//code.jquery.com/jquery-3.1.1.min.js", 100),
+            new Script("//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js", 200),
+            new Script("/Scripts.encyapp.webcore.js", 300)
         };
 
         public IEnumerable<MenuItem> MenuItems => new MenuItem[] { new MenuItem("/", "Home", 100) };
